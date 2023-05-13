fa805f76bbfa39220b3771c56649f5f0b006ca5a
Aluízio Catão <aluizio2210@gmail.com>
2023-05-13 11:36:05 -0500

    Iniciando a criação das Models

--- database/migrations/2023_05_13_002256_create_user_email_index_table.php
+++ database/migrations/2023_05_13_002256_create_user_email_index_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     {
         DB::unprepared("CREATE UNIQUE INDEX user_email_index 
                         ON users(email) 
-                        WHERE deleted_ad IS NULL
+                        WHERE deleted_at IS NULL
         ");
     }
 
